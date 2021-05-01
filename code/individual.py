a35b23a199bbbdb0c6b0c4f322af5d9d9c77032d
materight <matteo.est@gmail.com>
2021-05-01 19:09:19 +0200

    Update

---
+++ code/individual.py
@@ -0,0 +1,5 @@
+
+
+class Individual:
+    def __init__(self):
+        
--- main.py
+++ main.py
@@ -4,28 +4,20 @@ import sounddevice as sd
 import soundfile as sf
 import pretty_midi
 
-# Create a PrettyMIDI object
-cello_c_chord = pretty_midi.PrettyMIDI()
-# Create an Instrument instance for a cello instrument
-cello_program = pretty_midi.instrument_name_to_program('Cello')
-cello = pretty_midi.Instrument(program=cello_program)
-# Iterate over note names, which will be converted to note number later
-for i, note_name in enumerate(['C5', 'E5']):
-    # Retrieve the MIDI note number for this note name
-    note_number = pretty_midi.note_name_to_number(note_name)
-    # Create a Note instance for this note, starting at 0s and ending at .5s
-    note = pretty_midi.Note(velocity=1, pitch=note_number, start=i, end=i+1)
-    # Add it to our cello instrument
-    cello.notes.append(note)
-# Add the cello instrument to the PrettyMIDI object
-cello_c_chord.instruments.append(cello)
+
+
+chord = pretty_midi.PrettyMIDI()
+instrument = pretty_midi.Instrument(program=pretty_midi.instrument_name_to_program('Acoustic Grand Piano'))
+instrument.notes = [pretty_midi.Note(velocity=100, pitch=pitch, start=0, end=.5) for pitch in [60, 67, 64]]
+chord.instruments = [instrument]
 # Write out the MIDI data
-audio_data = cello_c_chord.synthesize()
+audio_data = chord.fluidsynth()
+
+
+midi_data = pretty_midi.PrettyMIDI('samples/scale_c_major.mid')
+audio_data = midi_data.fluidsynth()
 
 # Frames = audio length * sample rate
-
-data, fs = sf.read('samples/sample1.wav', frames=len(audio_data))
-data = data[:, 0] # TODO: convert to mono
+data, fs = sf.read('samples/scale_c_major.wav', frames=len(audio_data))
+data = data # TODO: convert to mono
 sd.play(audio_data, fs)
-
-fitness = (data - audio_data)**2
