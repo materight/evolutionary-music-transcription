bfab020d832705f489a17c4c2827cfe0d602c68a
materight <matteo.est@gmail.com>
2021-05-02 16:05:27 +0200

    Update

--- main.py
+++ main.py
@@ -6,23 +6,10 @@ import pretty_midi
 import itertools
 import inspyred
 from random import Random
+from time import time
 
 
 MIN_PITCH, MAX_PITCH = 21, 108
-
-target, fs = sf.read('samples/scale_c_major.wav', frames=int(4.5*44100))
-target_midi = pretty_midi.PrettyMIDI('samples/scale_c_major.mid')
-print(f'Target: {[n.pitch for n in target_midi.instruments[0].notes]}')
-
-#audio_data = midi_data.fluidsynth()
-
-chord = pretty_midi.PrettyMIDI()
-instrument = pretty_midi.Instrument(program=pretty_midi.instrument_name_to_program('Acoustic Grand Piano'))
-instrument.notes = [pretty_midi.Note(velocity=100, pitch=pitch, start=0, end=.5) for pitch in range(0,2000)]
-chord.instruments = [instrument]
-# Write out the MIDI data
-audio_data = chord.fluidsynth()
-
 
 def to_midi(candidate):
     midi = pretty_midi.PrettyMIDI()
@@ -33,8 +20,15 @@ def to_midi(candidate):
     return midi
 
 def compute_fitness(candidate, target_audio):
+    WND = 4096
     candidate_audio = to_midi(candidate).fluidsynth()
-    return np.sum((candidate_audio - target_audio) ** 2)
+    
+    pad_size = WND - (candidate_audio.size %  WND)
+    candidate_fft = np.abs(np.fft.rfft(np.pad(candidate_audio, (0, pad_size)).reshape((-1, WND)) * np.hanning(WND)))
+    target_fft = np.abs(np.fft.rfft(np.pad(target_audio, (0, pad_size)).reshape((-1, WND)) * np.hanning(WND)))
+    fitness = np.maximum(candidate_fft[:, 2:] / target_fft[:, 2:], target_fft[:, 2:] / candidate_fft[:, 2:]).sum()
+    # fitness = np.sum((candidate_audio - target_audio) ** 2)
+    return fitness
 
 def generator(random, args):
     size = args.get('num_notes')
@@ -46,35 +40,53 @@ def bounder(candidate, args):
         candidate[i] = min(max(pitch, MIN_PITCH), MAX_PITCH)
     return candidate
 
-@inspyred.ec.evaluators.evaluator
-def evaluator(candidate, args):
+def evaluator(candidates, args):
     size = args.get('num_notes')
-    return compute_fitness(candidate[:size], target)
+    target = args.get('audio_target')
+    fitnesses = []
+    for candidate in candidates:
+        fitnesses.append(compute_fitness(candidate[:size], target))
+    return fitnesses
    
 
 def observer(population, num_generations, num_evaluations, args):
-    print(f'{num_generations}) best: {population[0].fitness}  {np.array(population[0].candidate, dtype=int)}')
-
-rand = Random()
-rand.seed(42)
-es = inspyred.ec.ES(rand)
-es.variator = [inspyred.ec.variators.uniform_crossover, inspyred.ec.variators.gaussian_mutation]
-es.observer = observer
-es.terminator = [inspyred.ec.terminators.evaluation_termination, inspyred.ec.terminators.diversity_termination]
-final_pop = es.evolve(
-    generator=generator, 
-    evaluator=evaluator,
-    pop_size=100, 
-    bounder=bounder,
-    max_evaluations=3000,
-    maximize=False,
-    num_notes=7
-)
+    print(f'{num_generations}) best: {population[0].fitness:.2f}  {np.array(population[0].candidate, dtype=int)}')
+    if num_generations % 2 == 0:
+        print('Reproducing best individual...')
+        sd.play(to_midi(population[0].candidate).fluidsynth(), fs)
 
 
 
+if __name__ == '__main__':
+    target, fs = sf.read('samples/scale_c_major.wav', frames=int(4.5*44100))
+    target_midi = pretty_midi.PrettyMIDI('samples/scale_c_major.mid')
+    print(f'Target: {[n.pitch for n in target_midi.instruments[0].notes]}')
 
+    chord = pretty_midi.PrettyMIDI()
+    instrument = pretty_midi.Instrument(program=pretty_midi.instrument_name_to_program('Acoustic Grand Piano'))
+    instrument.notes = [pretty_midi.Note(velocity=100, pitch=pitch, start=0, end=.5) for pitch in range(0,2000)]
+    chord.instruments = [instrument]
+    # Write out the MIDI data
+    audio_data = chord.fluidsynth()
 
+    rand = Random()
+    rand.seed(time())
+    es = inspyred.ec.GA(rand)
+    es.variator = [inspyred.ec.variators.uniform_crossover, inspyred.ec.variators.gaussian_mutation]
+    es.observer = observer
+    es.terminator = [inspyred.ec.terminators.evaluation_termination, inspyred.ec.terminators.diversity_termination]
+    final_pop = es.evolve(
+        generator=generator, 
+        mp_evaluator=evaluator,
+        evaluator=inspyred.ec.evaluators.parallel_evaluation_mp,
+        mp_num_cpus=4,
+        pop_size=100, 
+        num_elites=2,
+        bounder=bounder,
+        max_evaluations=30000,
+        maximize=False,
+        num_notes=7,
+        audio_target=target
+    )
 
-# Frames = audio length * sample rate
-sd.play(audio_data, fs)
+    sd.play(audio_data, fs)
